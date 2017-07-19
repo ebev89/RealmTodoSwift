0fb7f74690bc821d6ef6a060c65c6902864d33fd
Egor <ebev89@mail.ru>
2017-07-19 19:04:19 +1000

    Initial Commit

---
+++ RealmTodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTodoApp
+//
+//  Created by Egor Tarasov on 7/19/17.
+//  Copyright © 2017 Neo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
